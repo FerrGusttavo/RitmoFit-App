eb6c20bfa309e4b1fe076422c9fd6d14e3a33ddb
Gustavo Augusto <gustavo.augusto.f@outlook.com>
2024-02-16 12:09:25 -0300

    create gyms usecase and check-ins usecase

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,11 +1,13 @@
-import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
-import { compare } from "bcryptjs";
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
+import { resourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInUseCaseRequest {
     userId: string
     gymId: string
+    userLatitude: number
+    userLongitude: number
 }
 
 interface CheckInUseCaseResponse {
@@ -13,10 +15,28 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-    constructor(private checkInRepository: CheckInsRepository) {}
+    constructor(
+        private checkInRepository: CheckInsRepository,
+        private gymsRepository: GymsRepository
+        ) {}
 
     async execute({ userId, gymId 
     }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+        const gym = await this.gymsRepository.findById(gymId)
+
+        if (!gym) {
+            throw new resourceNotFoundError()
+        }
+
+        const checkOnSameDate = await this.checkInRepository.findByUserIdOnDate(
+            userId,
+            new Date(),
+        )
+
+        if (checkOnSameDate) {
+            throw new Error()
+        }
+
         const checkIn = await this.checkInRepository.create({
             gym_id: gymId,
             user_id: userId,
